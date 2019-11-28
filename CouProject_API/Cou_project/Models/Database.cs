e6b1d15756d1cadc6ac9a59b073305076654f60e
Gormji <pierre.cardinal20@gmail.com>
2019-11-28 21:55:14 +0100

    commit

--- CouProject_API/Cou_project/Models/Database.cs
+++ CouProject_API/Cou_project/Models/Database.cs
@@ -5,7 +5,7 @@ namespace Cou_project.Models
     public static class DataBase
     {
         //MODIFIER POUR SON SERVEUR --> "@" pour le "\" qui se trouve dans le nom !!
-        public static readonly string SERVER = "DESKTOP-N27P2MS";
+        public static readonly string SERVER = @"LAPTOP-FRN65M2S\MSSQLSERVER01";
         
         private static readonly string CONNECTION_STRING = $"Server={SERVER};Database=cou_project;Integrated Security=SSPI;";
 
